eea03d0c3748fa9bf403046037ae62dfd10077d2
rajeshkonathala <rajesh21konathala@gmail.com>
2024-02-01 11:28:57 +0530

    added

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=(date +%F-%H-%M-%S)
+TIMESTAMP=(date +%F-%H-%M-%S) #ommnet
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "Script started executed at $TIMESTAMP" &>> $LOGFILE
