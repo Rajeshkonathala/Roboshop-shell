7fbe9b0bfb219b1016f33ebe292afd37b278981d
rajeshkonathala <rajesh21konathala@gmail.com>
2024-02-01 16:06:29 +0530

    Roboshop Shell Scripts

--- catalogue.sh
+++ catalogue.sh
@@ -28,67 +28,67 @@ else
     echo "you are root user"
 fi #fi means reverse of if, indication of condition end
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? " Disabling NodeJS"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 
 VALIDATE $? " Enabling NodeJS:18"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? " Installing NodeJS:18"
 
-useradd roboshop 
+useradd roboshop &>> $LOGFILE
 
 VALIDATE $? " Adding Roboshop User "
 
-mkdir /app
+mkdir /app &>> $LOGFILE
 
 VALIDATE $? " Creating app Directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? " Downloading Catalogue applicatiion"
 
 cd /app
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? " Unzipping Catalogue "
 
 cd /app
 
-npm install
+npm install &>> $LOGFILE
 
 VALIDATE $? " Installing NPM "
 
 #use absolute path, Because Catalogue.service is there
-cp /home/centos/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
 
 VALIDATE $? " Copying catalogue service file "
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 
 VALIDATE $? " Daemon Reload "
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGFILE
 
 VALIDATE $? " Enabling Catalogue "
 
-systemctl start catalogue
+systemctl start catalogue &>> $LOGFILE
 
 VALIDATE $? " Starting Catalogue "
 
-cp /home/centos/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? " Copying MongoDB Repo "
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? " Installing MongoDB Client "
 
-mongo --host 172.31.44.167 </app/schema/catalogue.js
+mongo --host 172.31.44.167 </app/schema/catalogue.js &>> $LOGFILE
 
-VALIDATE $? " Loading CATALOGUE data into MONGODB "
+VALIDATE $? "Loading CATALOGUE data into MONGODB"
